87a51c88842fe3f8f3d664240aacd25f244c0961
Aytugan Özgan <aytgnozgn@gmail.com>
2021-11-11 13:59:34 +0300

    array mutation

--- first/app.js
+++ first/app.js
@@ -1,41 +1,31 @@
-const name = "John";
-const age = 31;
-const job = "Web Developer";
-const city = "Miami";
-let html;
+//Create some arrays
+const numbers = [1, 2, 3, 4, 5];
+const numbers2 = new Array(6, 8, 7, 9);
+//MUTATING ARRAYS
+//add to end
+numbers.push(6);
+//add to start
+numbers.unshift(0);
+//remove last
+numbers.pop();
+//remove first
+numbers.shift();
+//remove between
+numbers.splice(1, 3);
+//reverse
+numbers.reverse();
+//concatenate arrays  [numbers + numbers2]
+let val = numbers.concat(numbers2);
+//sorting numbers
+val = numbers2.sort(); //sorting alphabetically !!
+val = numbers2.sort(function (x, y) {
+  return x - y;
+});
+//find first occurrence
+function under4(num) {
+  return num < 50;
+}
+val = numbers2.find(under4);
 
-//Without template strings
-html =
-  "<ul><li>Name: " +
-  name +
-  "  </li><li>Age:" +
-  age +
-  "</li><li>job: " +
-  job +
-  "</li> <li>city: " +
-  city +
-  "</li> </ul>";
-
-html =
-  "<ul>" +
-  "<li>Name:  " +
-  name +
-  "</li>" +
-  "<li>age:" +
-  age +
-  "</li>" +
-  "</ul>";
-
-//With template strings/literals (ES6)
-html = `
-<ul>
-    <li>Name: ${name} </li>
-    <li>Age: ${age} </li>
-    <li>Job: ${job} </li>
-    <li>City: ${city} </li>
-    <li>Lucky Number: ${2 * 2}</li>
-    <li>isYoung: ${age > 30 ? "no" : "yes"} </li>
-</ul>
-`;
-
-document.body.innerHTML = html;
+console.log(numbers);
+console.log(val);
