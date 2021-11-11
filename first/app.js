970eb4c57498c3be1f16077efc7c1e00f07a1e59
Aytugan Özgan <aytgnozgn@gmail.com>
2021-11-11 14:19:59 +0300

    dates

--- first/app.js
+++ first/app.js
@@ -1,31 +1,27 @@
-const person = {
-  firstName: "Steve",
-  lastName: "Smith",
-  age: 30,
-  email: "steve@aol.com",
-  hobbies: ["music", "sports"],
-  address: { city: "Miami", state: "FL" },
-  getBirthYear: function () {
-    return 2021 - this.age;
-  },
-};
+let val;
 
-let val = person;
+const today = new Date();
+let birthday = new Date("9-10-1981 11:25");
 
-//get specific value
-val = person.firstName;
-val = person["lastName"];
-val = person.address.city;
-val = person.address["city"];
-val = person.getBirthYear();
+birthday = new Date("Septembeer 10 1981");
+birthday = new Date("9/10/1982");
+
+val = today;
+val = birthday;
+val = today.getMonth(); //0 = January!
+val = today.getDate(); //1-31
+val = today.getDay(); //1 = Monday
+val = today.getFullYear();
+val = today.getHours();
+val = today.getMinutes();
+val = today.getSeconds();
+val = today.getMilliseconds();
+val = today.getTime(); // time passed since 1th January 1970 as seconds
+
+birthday.setMonth(2); //MARCH!
+birthday.setDate(12);
+birthday.setFullYear(1985)
+
 console.log(val);
-
-const people = [
-  { name: "John", age: 30 },
-  { name: "Mike", age: 23 },
-  { name: "Nancy", age: 22 },
-];
-
-for (let i = 0; i < people.length; i++) {
-  console.log(people[i].name);
-}
+console.log(birthday);
+console.log(typeof val);
