ec344cf7a2e772ae7cf14004ffedb2598335f7aa
Aytugan Özgan <aytgnozgn@gmail.com>
2021-11-11 11:45:18 +0300

    template literral

--- first/app.js
+++ first/app.js
@@ -1,42 +1,41 @@
-const firstName = "William";
-const lastName = "Johnson";
-const age = 36;
+const name = "John";
+const age = 31;
+const job = "Web Developer";
+const city = "Miami";
+let html;
 
-let val;
+//Without template strings
+html =
+  "<ul><li>Name: " +
+  name +
+  "  </li><li>Age:" +
+  age +
+  "</li><li>job: " +
+  job +
+  "</li> <li>city: " +
+  city +
+  "</li> </ul>";
 
-val = firstName + lastName;
+html =
+  "<ul>" +
+  "<li>Name:  " +
+  name +
+  "</li>" +
+  "<li>age:" +
+  age +
+  "</li>" +
+  "</ul>";
 
-//Concatenation
-val = firstName + " " + lastName;
-val = "Hello, my name is " + firstName + " and I am " + age;
-//Appending
-val = "Brad ";
-val += "Traversy";
-//Escaping
-val = "That's \"awesome\", I can't wait "; //escape with backSlash(\)
-//Length
-val = firstName.length; // length is property not method, so we dont use parenthesizes
-//concat
-val = firstName.concat(" ", lastName); // concatenation with method. add lastName to firstName between space that we defined as an argument
-//Change Case
-val = firstName.toUpperCase();
-val = firstName.toLowerCase();
+//With template strings/literals (ES6)
+html = `
+<ul>
+    <li>Name: ${name} </li>
+    <li>Age: ${age} </li>
+    <li>Job: ${job} </li>
+    <li>City: ${city} </li>
+    <li>Lucky Number: ${2 * 2}</li>
+    <li>isYoung: ${age > 30 ? "no" : "yes"} </li>
+</ul>
+`;
 
-val = firstName[0]; //first index of value, which is first character of our string (W)
-val = firstName.indexOf("w".toUpperCase()); // first occurrence of "W" which is first character which index is 0
-val = firstName.lastIndexOf("i"); //last occurrence of i which index of 4
-val = firstName.charAt("1"); // i which index is 1 (w index is 0)
-val = firstName.charAt(firstName.length - 1); //last character ( .legth starts with 1 but indexes start with 0)
-
-//Substring
-val = firstName.substring(0, 4); //not included index 4
-//Slice
-val = firstName.slice(0, 4);
-val = firstName.slice(-3); // start to slice from end, keep last 3
-//Split
-val = firstName.split(" "); // whenever space seen push it to an array and return that array
-//Replace
-val = firstName.replace('William',"Jack") //find "William" and replace it with "Jack"
-//Includes
-val = firstName.includes("William") //if includes, return true
-console.log(val);
+document.body.innerHTML = html;
