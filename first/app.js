724d85212e01986e77a256c0c1ec5d6a0d0f9773
Aytugan Özgan <aytgnozgn@gmail.com>
2021-11-13 15:05:42 +0300

    function scopes

--- first/app.js
+++ first/app.js
@@ -1,42 +1,22 @@
-// //shortcut
-// window.console.log(123);
-// console.log(123);
+//GLOBAL SCOPE
+var a = 1;
+let b = 2;
+const c = 3;
 
-// //Alert
-// alert("hi");
+(function test() {
+  var a = 4;
+  let b = 5;
+  const c = 6;
+  console.log("Function scope:", a, b, c);
+})();
 
-// //Prompt
-// const input = prompt();
-// console.log(input);
+if (true) {
+  //Block scope
+  var a = 4; //GLOBAL A!
+  let b = 5;
+  const c = 6;
 
-// // Confirm
-// const a = confirm("YER OR NO");
-// console.log(a);
+  console.log("Block scope: ", a, b, c);
+}
 
-let val;
-//Outer hight and width
-val = outerHeight;
-val = outerWidth;
-
-//Inner hight and width
-val = innerHeight;
-val = innerHeight;
-
-//Scroll points
-val = scrollY;
-val = scrollX;
-
-//Location Object
-val = location;
-//Redirect Reload
-// location.href = "http://google.com"
-// location.reload()
-
-//History Object
-// history.go(-1);
-val = history.length;
-
-//Navigator Object
-val = navigator;
-
-console.log(val);
+console.log("Global scope: ", a, b, c);
