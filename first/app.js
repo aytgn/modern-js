464e4469e7b229cc3b0c2c6a88f1c929d5ac8c3a
Aytugan Özgan <aytgnozgn@gmail.com>
2021-11-13 13:44:37 +0300

    functions

--- first/app.js
+++ first/app.js
@@ -1,38 +1,45 @@
-//switch
-const color = "blue";
+//FUNCTION DECELERATION
 
-switch (color) {
-  case "red":
-    console.log("Color is red");
-    break;
-  case "blue":
-    console.log("Color is blue");
-    break;
-  default:
-    console.log("Color is not red or blue");
-    break;
+function greet(firstName = "Armağan", lastName = "Şahin") {
+  return "hello" + " " + firstName + " " + lastName + "!";
 }
 
-let day;
-switch (new Date().getDay()) {
-  case 0:
-    day = "Sunday";
-    break;
-  case 1:
-    day = "Monday";
-    break;
-  case 2:
-    day = "Thuesday";
-    break;
-  case 3:
-    day = "Wednesday";
-    break;
-  case 6:
-    day = "Saturday";
-    break;
-  default:
-    day = "aga";
-    break;
-}
+const myGreetingMessage = greet("Aytugan", "Özgan");
+const greetDefaultUser = greet();
 
-console.log(day)
+console.log(myGreetingMessage);
+console.log(greetDefaultUser);
+
+//FUNCTION EXPRESSIONS
+
+const square = function (x = 3) {
+  return x * x;
+};
+
+console.log(square(8));
+console.log(square());
+
+//IMMEDIATELY INVOKABLE FUNCTION EXPRESSIONS - IIFEs
+
+(function (myName) {
+  console.log("i called immediately!");
+  console.log("and my name is " + myName + "!");
+})("aga");
+
+//PROPERTY METHODS
+
+const todo = {
+  add: function () {
+    console.log("Add todo..");
+  },
+  edit: function (id) {
+    console.log("Edit todo id: " + id);
+  },
+};
+todo.delete = function () {
+  console.log("Delete todo...");
+};
+
+todo.add();
+todo.edit(1);
+todo.delete();
